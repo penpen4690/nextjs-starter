e963bf25d6476c24ade1a256acd8723ac879a66d
優季 石尾 <ishio@uchida-it.co.jp>
2021-12-17 17:16:39 +0900

    Gamesにリストを表示する

--- pages/contents/game.js
+++ pages/contents/game.js
@@ -1,4 +1,6 @@
 import { projects } from '../../utils/projectsData'
+import { games } from '../../utils/gamesData'
+import SmallCard from '../../components/SmallCard';
 
 const Game = () => {
     return (
@@ -21,6 +23,12 @@ const Game = () => {
             </aside>
             <main>
                 <h1>Games</h1>
+                <div className="contents-card-grid">
+                    {games.map((game) => {
+                        return <SmallCard key={game.id} Icon={'react'} title={game.name} slug={game.slug} />;
+                    })}
+                </div>
+
             </main>
         </div>
     )
---
+++ utils/gamesData.js
@@ -0,0 +1,8 @@
+const games = [
+    { id: 'ebikani', name: 'えびかに', slug: 'ebikani'},
+    { id: 'usakame', name: 'うさぎとかめ', slug: 'usakame' },
+    { id: 'arikiri', name: 'ありときりぎりす', slug: 'arikiri'}
+  ];
+  
+  module.exports = { games };
+  
