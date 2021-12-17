89c9d4290b251a87560c6f6c56e3e6ad36431fb4
優季 石尾 <ishio@uchida-it.co.jp>
2021-12-17 16:38:04 +0900

    projectsのidを変更

--- utils/projectsData.js
+++ utils/projectsData.js
@@ -1,25 +1,7 @@
 const projects = [
-  {
-    id: 'react',
-    name: 'React',
-    path: 'facebook/react',
-    slug: 'facebook-react',
-  },
-  { id: 'vue', name: 'Vue', path: 'vuejs/vue', slug: 'vuejs-vue' },
-  {
-    id: 'svelte',
-    name: 'Svelte',
-    path: 'sveltejs/svelte',
-    slug: 'sveltejs-svelte',
-  },
-  { id: 'next', name: 'Next.js', path: 'zeit/next.js', slug: 'zeit-nextjs' },
-  { id: 'nuxt', name: 'Nuxt.js', path: 'nuxt/nuxt.js', slug: 'nuxt-nuxtjs' },
-  {
-    id: 'gatsby',
-    name: 'Gatsby',
-    path: 'gatsbyjs/gatsby',
-    slug: 'gatsbyjs-gatsby',
-  },
+  { id: 'aboutme', name: 'ABOUT ME', path: 'facebook/react', slug: 'facebook-react'},
+  { id: 'blog', name: 'BLOG', path: 'vuejs/vue', slug: 'vuejs-vue' },
+  { id: 'game', name: 'GAME', path: 'sveltejs/svelte', slug: 'sveltejs-svelte'}
 ];
 
 module.exports = { projects };
